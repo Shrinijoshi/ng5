0e05b9069e66c47936b1d9f47240482dc5d91196
Shrinivas Joshi <josh.shrinivas@gmail.com>
2018-02-18 13:11:35 +0530

    first-commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations'
+import { DataService } from '../data.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals',[
+      transition('*=>*',[
+        query(':enter',style({opacity: 0}),{optional:true}),
+
+        query(':enter', stagger('300ms',[
+          animate('.6s ease-in-out', keyframes([
+            style({opacity:0, transform:'translateY(-75%)', offset:0}),
+            style({opacity:.5, transform:'translateY(35px)', offset:.3}),
+            style({opacity:1, transform:'translateY(0)', offset:1})
+          ]))]),{optional:true}),
+          
+          query(':leave', stagger('300ms',[
+            animate('.6s ease-in-out', keyframes([
+              style({opacity:1, transform:'translateY(0)', offset:0}),
+              style({opacity:.5, transform:'translateY(35px)', offset:.3}),
+              style({opacity:0, transform:'translateY(-75%)', offset:1})
+            ]))]),{optional:true}) 
+      ])
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+  itemCount: number;
+  btnText: string = 'Add an Item';
+  goalText:string = 'My First Life Goal';
+  goals = [];
+
+  constructor( private _data:DataService ) { 
+    
+  }
+
+  ngOnInit() {
+  
+    this._data.goal.subscribe(res=>this.goals = res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
+  }
+
+  addItem(){
+    this.goals.push(this.goalText)
+    this.goalText=''
+    this.itemCount = this.goals.length
+    this._data.changeGoal(this.goals);
+  }
+
+  removeItem(i){
+    this.goals.splice(i,1)
+    this._data.changeGoal(this.goals);
+  }
+}
